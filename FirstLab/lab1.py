a6b3f4a73e777e7dd2e46273294e81513d8b6bcb
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2024-01-20 12:08:29 -0500

    initial commit

---
+++ FirstLab/lab1.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+
+df = pd.read_csv("FuelConsumptionCo2.csv")
+
+# take a look at the dataset
+df.head()
+
+

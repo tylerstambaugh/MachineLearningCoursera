4602531e7b2f96cf6eda85f164a640fc94857d58
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2024-01-21 06:54:45 -0500

    learning

--- FirstLab/lab1.py
+++ FirstLab/lab1.py
@@ -3,9 +3,12 @@ import pandas as pd
 import pylab as pl
 import numpy as np
 
-df = pd.read_csv("FuelConsumptionCo2.csv")
+file_path = r'C:\Users\tstambaugh\source\repos\MachineLearning\FirstLab\FuelConsumptionCo2.csv'
+
+df = pd.read_csv(file_path)
 
 # take a look at the dataset
 df.head()
 
-
+# summarize the data
+df.describe()

0a3b3655728e9dbdebb3de85c19d6fdecdb5056e
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2024-01-23 14:57:20 -0500

    k nearest neighbor lab

---
+++ KNN/knn_lab.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+
+df = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv')
+df.head()
+
+df['custcat'].value_counts()
+
+df.hist(column='income', bins=50)
+
+df.columns
+
+X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
+X[0:5]
+
+y = df['custcat'].values
+y[0:5]
+
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+X[0:5]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+k = 4
+#Train Model and Predict  
+neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+neigh
+
+#We can use the model to make predictions on the test set:
+yhat = neigh.predict(X_test)
+yhat[0:5]
+
